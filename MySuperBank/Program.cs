eae4c3c18f2cae16487cab53a86208d2d7bf8cea
andrewy9 <andrew.yang993@gmail.com>
2021-04-09 19:43:10 +1200

    initial commit

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Andrew", 1000m);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(100m, DateTime.Now, "PS5");
+            account.MakeWithdrawal(200m, DateTime.Now, "PS6");
+            account.MakeWithdrawal(300m, DateTime.Now, "PS7");
+
+        }
+    }
+}
